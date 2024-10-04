f4631e01202f13d62148d7f883a2e7b9251d9164
Talgat <ttokayev7@gmail.com>
2024-10-04 20:25:23 +0500

    screens

---
+++ app/components/screens/materials/Materials.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Materials = () => {
+  return (
+    <View>
+      <Text>Materials</Text>
+    </View>
+  )
+}
+
+export default Materials
---
+++ app/components/screens/techniques/Techniques.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Techniques = () => {
+  return (
+    <View>
+      <Text>Techniques</Text>
+    </View>
+  )
+}
+
+export default Techniques
