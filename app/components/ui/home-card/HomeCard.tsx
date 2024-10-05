82e38a98f036cdb590a2a30c481d12d25979f964
Talgat <ttokayev7@gmail.com>
2024-10-05 23:49:51 +0500

    UI Home Card

---
+++ app/components/ui/home-card/HomeCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'; 
+import { View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { TypeFeatherIconNames } from '@/types/icon.interface';
+
+interface HomeCardProps {
+  icon: TypeFeatherIconNames;
+  iconColor: string;
+  title: string;
+  description: string;
+  bgColor: string;
+}
+
+const HomeCard: React.FC<HomeCardProps> = ({ icon, iconColor, title, description, bgColor }) => {
+  return (
+    <View className='rounded-lg shadow-md p-4 mb-4 w-full flex-row items-center' style={{ backgroundColor: bgColor }}>
+      <View className='flex-1 mr-4'>
+        <Text className='text-xl font-semibold text-black'>{title}</Text>
+        <Text className='mt-2 text-md text-gray-700'>{description}</Text>
+      </View>
+      <View className='flex items-center justify-center' style={{ padding: 10, marginRight: 10 }}>
+        <Feather name={icon} size={40} color={iconColor} />
+      </View>
+    </View>
+  );
+};
+
+export default HomeCard;
