4dd13c80a6b094fb4e2c2dc8dcb86baafc4bc919
Talgat <ttokayev7@gmail.com>
2024-10-05 22:04:10 +0500

    Разделил на приватыне и публичные экраны

--- app/navigation/Navigation.tsx
+++ app/navigation/Navigation.tsx
@@ -2,23 +2,32 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TypeRootStackParamList } from "./navigation.types";
 import { FC } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { routes } from "./routes";
+import { publicRoutes, routes } from "./routes";
+import { useAuth } from "@/context/AuthContext";
 
 const Stack = createNativeStackNavigator<TypeRootStackParamList>();
 
 const Navigation:FC = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown: false,
-        contentStyle: {backgroundColor: '#ffffff'}
-      }}>
-        {
-          routes.map(route => <Stack.Screen key={route.name} {...route} />)
-        }
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: '#ffffff' },
+        }}
+      >
+        {isAuthenticated
+          ? routes.map((route) => (
+              <Stack.Screen key={route.name} {...route} />
+            ))
+          : publicRoutes.map((route) => (
+              <Stack.Screen key={route.name} {...route} />
+            ))}
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default Navigation
